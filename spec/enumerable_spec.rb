126c9a6ab14dbde1cb056abe77db03473afbbcbe
Roy Nyaga <nyagaandreroy@gamil.com>
2019-08-23 22:32:51 +0100

    created more test cases for my-each-with-index, my-select, my-all and my-any

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -23,6 +23,13 @@ RSpec.describe Enumerable do
             arr.my_each_with_index { |elem, index| sum += elem + index}
             expect(sum).to eql(20)
         end
+
+        it 'returns a array of concatenated strings with their index' do
+            arr = ['a', 'b', 'c', 'd', "e"]
+            concat = ""
+            arr.my_each_with_index { |elem, index| concat += "#{elem}#{index}" }
+            expect(concat).to eql("a0b1c2d3e4")
+        end
     end
     describe ' #my_select' do
         it ' iterates and select odd numbers in an array' do
@@ -30,12 +37,24 @@ RSpec.describe Enumerable do
             arr_select = arr.my_select { |elem| elem.odd? }
             expect(arr_select).to match_array([1,3,5,7,9])
         end
+
+        it 'iterates and returns string of lenght 5' do 
+            arr2 = ["how", "there", "many", "carry", "taken", "yes"]
+            arr_select2 = arr2.my_select { |word| word.length == 5 }
+            expect(arr_select2).to match_array(["there", "carry", "taken"]) 
+        end 
     end
     describe ' #my_all?' do
-        it ' iterates and compare all numbers in an array with a condition true' do
+        it 'returns elements less than 10' do
             arr = [0,1,2,3,4,5,6,7,8,9]
             cond = arr.my_all? { |elem| elem < 10 }
             expect(cond).to be true
+        end
+
+        it ' returns true when all elements start with s' do
+            arr = ['school', 'soccer', 'sugar', 'rain']
+            cond = arr.my_all? { |elem| elem[0] == 's' }
+            expect(cond).to be false
         end
     end
     describe ' #my_any?' do
@@ -43,6 +62,12 @@ RSpec.describe Enumerable do
             arr = [0,1,2,3,4,5,6,7,8,9]
             cond = arr.my_any? { |elem| elem == 4 }
             expect(cond).to be true
+        end
+
+        it ' iterates and compare if any element in the array have the condition in the block' do
+            arr = ['word', 'school', 'bus', 'eat']
+            cond = arr.my_any? { |elem| elem == 'take' }
+            expect(cond).to be false
         end
     end
     describe '#my_none?' do
